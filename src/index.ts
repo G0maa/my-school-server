6cb83bc164f3f3d88fe03e5dfdeb3de0d7131cd4
G0maa <midomaxgomaa@gmail.com>
2022-12-01 16:24:22 +0200

    Use of PostgreSQL and its Migrations

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import http from 'http';
 import app from './app';
 import config from './utils/config';
+import { connectToDatabase } from './utils/db';
 import logger from './utils/logger';
 
 const server = http.createServer(app);
 
-const { PORT } = config;
-server.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`);
-  logger.info(`URL: http://localhost:${PORT}`);
-  logger.info(`Running environment: ${config.NODE_ENV}`);
-});
+const start = async () => {
+  // Here, or in app.ts?
+  await connectToDatabase();
+
+  server.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`);
+    logger.info(`URL: http://localhost:${config.PORT}`);
+    logger.info(`Running environment: ${config.NODE_ENV}`);
+  });
+};
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+start();
---
+++ src/types.ts
@@ -0,0 +1,8 @@
+import { QueryInterface } from 'sequelize';
+import { MigrationParams } from 'umzug';
+
+// This is from the docs of umzug.. sort of.
+// https://github.com/sequelize/umzug
+export type Migration = (
+  params: MigrationParams<QueryInterface>
+) => Promise<unknown>;
