4bc5548c7a632048d29d5e9d76344d742efa9914
Mohammed Gomaa <midomaxgomaa@gmail.com>
2022-12-23 17:33:45 +0200

    Ci (#21)
    * A CI Pipeline using Github Actions that does:
    * Build
    * Lint & Format
    * API Testing
    * Post Code coverage on PR

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  roots: ['<rootDir>/src'],
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: ['**/src/**/**', '!**/src/migrations/**'], // This may not reflect actual coverage.
+  coverageProvider: 'v8',
+  coverageReporters: ['cobertura', 'text', 'text-summary'],
   globalSetup: './src/tests/setup.ts',
   globalTeardown: './src/tests/teardown.ts',
 };
