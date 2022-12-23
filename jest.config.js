3d639b4545acef91ed98c5df935e16684d60a7d8
G0maa <midomaxgomaa@gmail.com>
2022-12-23 10:41:17 +0200

    Preparing for CI: Code coverage

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: ['**/src/**/**', '!**/src/migrations/**'], // This may not reflect actual coverage.
+  coverageProvider: 'v8',
+  coverageReporters: ['lcov', 'text', 'text-summary'],
   globalSetup: './src/tests/setup.ts',
   globalTeardown: './src/tests/teardown.ts',
 };
