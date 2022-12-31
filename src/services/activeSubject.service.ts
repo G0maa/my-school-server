33025dd54765a9fae0235258fe48e45d3b4689f1
G0maa <midomaxgomaa@gmail.com>
2022-12-31 18:37:46 +0200

    activeSubject search

--- src/services/activeSubject.service.ts
+++ src/services/activeSubject.service.ts
@@ -1,19 +1,19 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import ActiveSubject from '../models/activeSubject';
-import { ZActiveSubject } from '../validator/activeSubject.validator';
+import {
+  ZActiveSubject,
+  ZActiveSubjectQuery,
+} from '../validator/activeSubject.validator';
 
 // #17 very WET CRUD operations.
-const getActiveSubjects = async () => {
-  const query = await ActiveSubject.findAll();
+// Should I incldie Teacher, studyClass, & Subject?
+// Searching not tested
+const getActiveSubjects = async (searchQuery: ZActiveSubjectQuery) => {
+  const query = await ActiveSubject.findAll({ where: { ...searchQuery } });
   return query;
 };
 
-// Lots of variations in this one.
-// To-Do
-// /api/activeSubjects/:id?query=teacher i.e. all classes & subjects for this teacher id
-// /api/activeSubjects/:id?query=studyClass i.e. all subjects & teachers for this class
-// /api/activeSubjects/:id?query=subject i.e. all classes & teachers for this subject
 const getActiveSubject = async (serial: string) => {
   const query = await ActiveSubject.findOne({
     where: { serial },
