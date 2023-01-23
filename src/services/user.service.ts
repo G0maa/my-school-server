f8eacc5f7b07b30d9e1cb1afa954a6d17a9d1c2a
G0maa <midomaxgomaa@gmail.com>
2023-01-23 16:46:06 +0200

    Refactoring user services

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,5 +1,36 @@
-import { User } from '../models';
-import { ZUserPut } from '../validator/user.validator';
+import { Student, Teacher, User, Admin } from '../models';
+import UserDetails from '../models/userDetails';
+import { ZStudent } from '../validator/student.validator';
+import { ZTeacher } from '../validator/teacher.validator';
+import { ZUser, ZUserPut } from '../validator/user.validator';
+import { ZUserDetails } from '../validator/userDetails.validator';
+
+type ZModel = ZTeacher | ZStudent;
+const childModels = { Student, Teacher, Admin };
+// Testing this out
+const createUser = async (
+  zUser: ZUser,
+  zUserDetails: ZUserDetails,
+  zModel: ZModel
+) => {
+  // Dealt with in route?
+  // zUser.role = 'Student';
+
+  const childModel = zUser.role.toLowerCase();
+
+  const user = await User.create(
+    {
+      ...zUser,
+      [childModel]: { ...zModel },
+      userDetails: { ...zUserDetails },
+    },
+    {
+      include: [childModels[zUser.role], UserDetails],
+    }
+  );
+
+  return user;
+};
 
 const updateUser = async (zUser: ZUserPut) => {
   const user = await User.findOne({ where: { id: zUser.id } });
@@ -16,4 +47,4 @@ const deleteUser = async (id: string) => {
   await User.destroy({ where: { id } });
 };
 
-export { updateUser, deleteUser };
+export { createUser, updateUser, deleteUser };
