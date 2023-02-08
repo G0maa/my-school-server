06d809f3cd2ec7dfa96bc237e1861824b4c62deb
G0maa <midomaxgomaa@gmail.com>
2023-02-08 11:59:57 +0200

    Refacor: use of zod in holiday controller

--- src/services/holiday.service.ts
+++ src/services/holiday.service.ts
@@ -1,18 +1,18 @@
 import Holiday from '../models/holiday';
-import { ZHoliday, ZHolidaySerial } from '../validator/holiday.validator';
+import { ZHolidayDelete, ZHolidayPost } from '../validator/holiday.validator';
 
 const getHolidays = async () => {
   const holidays = await Holiday.findAll();
   return holidays;
 };
 
-const createHoliday = async (zHoliday: ZHoliday) => {
+const createHoliday = async (zHoliday: ZHolidayPost['body']) => {
   const holiday = await Holiday.create({ ...zHoliday });
   return holiday;
 };
 
 // There are no errors/messages given when you delete non-existent resource.
-const deleteHoliday = async (serial: ZHolidaySerial) => {
+const deleteHoliday = async (serial: ZHolidayDelete['params']['serial']) => {
   const holiday = await Holiday.findOne({ where: { serial } });
   if (!holiday) return;
   await holiday.destroy();
